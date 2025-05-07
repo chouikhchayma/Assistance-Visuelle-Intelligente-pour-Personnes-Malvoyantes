7335d91d521e42994e7b241056e4940143f6d9d2
chouikhchayma <155112117+chouikhchayma@users.noreply.github.com>
2025-05-07 21:08:50 +0200

    Delete lib/binding directory

--- lib/binding/detection_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ocr/controller/detection_controller.dart';
-import 'package:get/get.dart';
-
-class DetectionBinding extends Bindings{
-  @override
-  void dependencies() {
-    Get.lazyPut(() => DetectionController());
-  }
-}
